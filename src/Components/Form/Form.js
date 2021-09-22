b6422c563014613dc4e5ce76ebd5ace856eb83dc
Katrin <filmprof34@gmail.com>
2021-09-22 15:38:25 +0300

    desktop goodforms

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -5,16 +5,11 @@ export default function Form() {
     <div className="container">
         <div>
             <form action="#" className="form-box">
-                <select name="Destination" placeholder="Destination" className="form-item">
-                    <option value="Destination" >Destination</option>
-                    <option value="England">Англия</option>
-                    <option value="USA">США</option>
-                    <option value="Canada">Канада</option>
-                </select>
-                <input type="date" placeholder="Check-in" className="form-item"/>
-                <input type="date" placeholder="Check-out" className="form-item"/>
-                <input type="number" placeholder="Adults" className="form-item"/>
-                <input type="number" placeholder="Children" className="form-item"/>
+                <input type="text"placeholder="Destination" className="form-item "/><span className="marked-input"></span>
+                <input type="text"  placeholder="Check-in" className="form-item"/>
+                <input type="text" placeholder="Check-out" className="form-item"/>
+                <input type="number" placeholder="Adults" className="form-item mini-input"/>
+                <input type="number" placeholder="Children" className="form-item mini-input"/>
                 <button className="button-search">search</button>
             </form>
         </div>
